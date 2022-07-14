8d0688f51141598062bbdb1ebf0c183ace2cb4b7
Sekiyu19 <yuto200248@gmail.com>
2022-07-14 23:37:34 +0900

    HoT: initial revision

---
+++ HeadsOrTails.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.Random;
+public class HeadsOrTails{
+    public static void main(String[] args) {
+		Random rand = new Random();
+		BodyParts[] bodyParts = new BodyParts[3];
+		for (int i = 0; i < bodyParts.length; i++){
+			bodyParts[i] = (rand.nextInt(2) == 0) ? new Head() : new Tail();
+			System.out.println("Round "  + (i + 1) + ": " + bodyParts[i].getName());
+		}
+		System.out.println("Heads: " + Head.getCount() + ", Tails: " + Tail.getCount());
+    }
+}
+
+abstract class BodyParts {
+	String name;
+    abstract String getName();
+}
+
+class Head extends BodyParts {
+    private static int count;
+	String name = "Heads";
+    public Head() {};
+    public String getName() {
+		increment();
+		return name;
+    }
+    public static void increment() { count++; }
+    public static int getCount() { return count; }
+}
+
+class Tail extends BodyParts {
+    private static int count;
+	String name = "Tails";
+    public Tail() {};
+    public String getName() {
+		increment();
+		return name;
+    }
+    public static void increment() { count++; }
+    public static int getCount() { return count; }
+}
